74c27dcff822a5330cfbb09494a15b9d295d2a0f
Rich Hildred <rhildred@gmail.com>
2022-03-29 11:19:27 -0400

    tiny bit of scaffold

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class Question extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>{this.props.question.text}</Text>
+      </View>
+
+    );
+  }
 }
 
+class App extends React.Component {
+  constructor(props) {
+
+    const questions = [
+      {text: "What is your name?"},
+      {text: "What color is the sky?"}
+    ] 
+    super(props);
+
+    this.state = {
+
+      question: questions[1]
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Question question={this.state.question} />
+      </View>
+    );
+  }
+}
+
+// This a method of making cleaner code. It acts similarly to a CSS file would in HTML. But we won’t use it much in this tutorial
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +45,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
